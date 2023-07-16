dc59f115ae6681efb53716491bb139b4b2a46193
aadhil96 <aadhil.imam@gmail.com>
2023-07-16 22:45:18 +0530

    data transformation done

--- src/utils.py
+++ src/utils.py
@@ -4,15 +4,16 @@ import dill
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
-
-def save_object(file_path,obj):
+import dill
+import pickle
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
 
-        os.mkdir(dir_path,exist_ok=True)
+        os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path,"wb") as file_obj:
-            dill.dump(obj,file_obj)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
